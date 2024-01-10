291a1624f58ece5621f5b537fa85f96a387db635
simces <cesnaviciuss@gmail.com>
2024-01-10 14:13:34 +0200

    fixed structure

---
+++ backend/src/test/java/com/photoAI/PhotoAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
